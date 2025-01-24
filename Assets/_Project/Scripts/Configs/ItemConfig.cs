84404cca003fc3d49a6ca291222245700c194f3c
guilty <guiltyvlr4u@gmail.com>
2025-01-24 19:59:05 +1000

    Pure List

--- Assets/_Project/Scripts/Configs/ItemConfig.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace _Project.Scripts.Configs
-{
-    public abstract class ItemConfig : ScriptableObject
-    {
-        [field: SerializeField] public int Weight { get; private set; }
-    }
-}
